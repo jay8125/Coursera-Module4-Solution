ec619fec15de40e4d1a728a1a62a60aede33cbab
GOLLA JAYANTH <61387084+jay8125@users.noreply.github.com>
2020-10-17 08:34:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (function() {
-    var name = ["Jayanth", "Joy", "Yaakov", "Golla", "Bindu"];
+    var name = ["Yaakov","John","Jen","Jason","Paul","Frank","Larry","Paula","Laura","Jim"];
     for (var i = 0; i < name.length; i++) {
         var firstLetter = name[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
